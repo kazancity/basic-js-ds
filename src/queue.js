c81e67429499e995022999cdb8f2e7c290638cbe
Artem Banderov <artem_75@bk.ru>
2022-05-02 20:56:53 +0300

    Delete queue.js

--- src/queue.js
+++
@@ -1,4 +0,0 @@
-const{ListNode}=require('../extensions/list-node.js');
-exports.Queue=class Queue{getUnderlyingList(){return this.hd}
-enqueue(value){var node=new ListNode(value);if(!this.hd){this.hd=node}else{var cr=this.hd;
-while(cr){if(!cr.next){cr.next=node;break}cr=cr.next}}}dequeue(){var z=this.hd.value;this.hd=this.hd.next;return z}}
