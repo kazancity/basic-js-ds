25732d0adf9bf757a423944dbae8cdcd693b76ce
kazancity <kazancity@aol.com>
2022-05-02 20:53:27 +0300

    init: start queue task

--- src/queue.js
+++ src/queue.js
@@ -1,36 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
-
-module.exports = {
-  Queue
-};
+const{ListNode}=require('../extensions/list-node.js');
+exports.Queue=class Queue{getUnderlyingList(){return this.hd}
+enqueue(value){var node=new ListNode(value);if(!this.hd){this.hd=node}else{var cr=this.hd;
+while(cr){if(!cr.next){cr.next=node;break}cr=cr.next}}}dequeue(){var z=this.hd.value;this.hd=this.hd.next;return z}}
